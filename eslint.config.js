18c093a88a36a6089660a63ffb87bbeb3b681c11
Raul Alfredo Chin Caamal <raul.chim@macropay.mx>
2025-12-26 09:50:48 -0600

    chore(Root): Modified Eslint Config again 😀

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,70 @@
-import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
 import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import { defineConfig } from "eslint/config"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
 
-export default defineConfig([
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
   {
-    ignores: ["node_modules", "**/node_modules/**", "**/*.js", "**/*.d.ts"],
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+    ignores: [
+      "node_modules",
+      "**/node_modules/**",
+      "**/dist",
+      "**/*.js",
+      "**/*.d.ts",
     ],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended"
+  ),
+  {
+    plugins: {
+      prettier,
+      "@typescript-eslint": typescriptEslint,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tsParser,
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    rules: {
+      "prefer-const": "error",
+      "prettier/prettier": "error",
+      "react-hooks/exhaustive-deps": 0,
+      "import/order": 0,
+      "no-debugger": "error",
+      "no-var": "error",
+      "no-use-before-define": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "no-warning-comments": "error",
+      "no-inline-comments": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        {
+          allowSingleExtends: true,
+        },
+      ],
+      "@typescript-eslint/ban-types": 0,
+    },
   },
-])
+]
