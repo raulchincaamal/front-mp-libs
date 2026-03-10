786812b08d5a182953323e4dce8a3e4c178129b9
Raul Alfredo Chin Caamal <raul.chim@macropay.mx>
2026-03-10 13:40:44 -0600

    build: update storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,115 +1,5 @@
-// import prettier from "eslint-plugin-prettier"
-// import typescriptEslint from "@typescript-eslint/eslint-plugin"
-// import globals from "globals"
-// import tsParser from "@typescript-eslint/parser"
-// import path from "node:path"
-// import { fileURLToPath } from "node:url"
-// import { FlatCompat } from "@eslint/eslintrc"
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-//   recommendedConfig: js.configs.recommended,
-//   allConfig: js.configs.all,
-// })
-
-// import { defineConfig, globalIgnores } from "eslint/config"
-// import nextVitals from "eslint-config-next/core-web-vitals"
-// import nextTs from "eslint-config-next/typescript"
-// import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
-
-// export default [
-//   {
-//     ignores: [
-//       "node_modules",
-//       "**/node_modules/**",
-//       "**/dist",
-//       "**/*.js",
-//       "**/*.d.ts",
-//     ],
-//   },
-//   ...compat.extends(
-//     "eslint:recommended",
-//     "prettier",
-//     "plugin:@typescript-eslint/recommended"
-//   ),
-//   {
-//     plugins: {
-//       prettier,
-//       "@typescript-eslint": typescriptEslint,
-//     },
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//       },
-//       parser: tsParser,
-//       parserOptions: {
-//         tsconfigRootDir: import.meta.dirname,
-//       },
-//     },
-//     rules: {
-//       "prettier/prettier": "error",
-//       "prefer-const": "error",
-//       "react-hooks/exhaustive-deps": 0,
-//       "import/order": 0,
-//       "@typescript-eslint/no-unused-vars": [
-//         "error",
-//         {
-//           argsIgnorePattern: "^_",
-//           varsIgnorePattern: "^_",
-//           caughtErrorsIgnorePattern: "^_",
-//         },
-//       ],
-//       "@typescript-eslint/no-explicit-any": "error",
-//       "@typescript-eslint/no-empty-interface": [
-//         "error",
-//         {
-//           allowSingleExtends: true,
-//         },
-//       ],
-//       "@typescript-eslint/ban-types": 0,
-//       "@typescript-eslint/consistent-type-imports": [
-//         "error",
-//         {
-//           prefer: "type-imports",
-//         },
-//       ],
-//       "no-console": "error",
-//       "@typescript-eslint/ban-types": 0,
-//       "max-len": [
-//         "error",
-//         {
-//           code: 80,
-//           ignoreComments: true,
-//           ignoreStrings: true,
-//           ignoreTemplateLiterals: true,
-//           ignoreRegExpLiterals: true,
-//         },
-//       ],
-//       "max-lines": [
-//         "error",
-//         {
-//           max: 80,
-//           skipBlankLines: true,
-//           skipComments: true,
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     files: [
-//       "**/*.stories.tsx",
-//       "**/*.test.tsx",
-//       "./packages/icons/lib/**/index.ts",
-//     ],
-//     rules: {
-//       "max-lines": "off",
-//     },
-//   },
-// ]
-
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook"
 import { defineConfig, globalIgnores } from "eslint/config"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import tseslint from "typescript-eslint"
@@ -186,6 +76,7 @@ const eslintConfig = defineConfig([
     "**/*.js",
     "**/*.d.ts",
   ]),
+  ...storybook.configs["flat/recommended"],
 ])
 
 export default eslintConfig
--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,17 +1,19 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-themes",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-themes"),
+    getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-vitest"),
   ],
 
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
 
@@ -21,3 +23,7 @@ const config: StorybookConfig = {
 }
 
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
